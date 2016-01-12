1bd4141077fff2f7cd460dad663c39fe1eef4ac7
aitap <krylov.r00t@gmail.com>
2016-01-12 22:47:17 +0300

    fix display of 'possible precipitation/storm' icons

--- weather.pl
+++ weather.pl
@@ -25,8 +25,8 @@ binmode STDOUT, ":encoding(locale)";
 my %phenomena = (
 	cloudiness => {name => "облачность", 0 => "\N{BLACK SUN WITH RAYS}", 1 => "\x{1f324}", 2 => "\x{1f325}", 3 => "\N{CLOUD}"},
 	precipitation => {name => "осадки", 4 => "\N{UMBRELLA}", 5 => "\N{UMBRELLA WITH RAIN DROPS}", 6 => "\N{SNOWFLAKE}", 7 => "\N{SNOWFLAKE}", 8 => "\x{1f329}", 9 => "?", 10 => " "},
-	rpower => {0 => "\x{1f327}?", 1 => "\x{1f327}!"},
-	spower => {0 => "\x{26c8}?", 1 => "\x{26c8}!"},
+	rpower => {0 => "?\x{1f327}", 1 => "!\x{1f327}"},
+	spower => {0 => "?\x{26c8}", 1 => "!\x{26c8}"},
 );
 
 
@@ -34,13 +34,13 @@ my @wind = (qw(N NE E SE S SW W NW));
 
 print "Прогноз для: ", decode(cp1251 => uri_unescape($data->{REPORT}{TOWN}{sname})), ", широта $data->{REPORT}{TOWN}{latitude}, долгота $data->{REPORT}{TOWN}{longitude}\n";
 for my $forecast (@{$data->{REPORT}{TOWN}{FORECAST}}) {
-	printf "%04d-%02d-%02d:%02d", $forecast->{year}, $forecast->{month}, $forecast->{day}, $forecast->{hour}; #, $forecast->{predict};
+	printf "%04d-%02d-%02d:%02d", $forecast->{year}, $forecast->{month}, $forecast->{day}, $forecast->{hour};
 	
 	for (qw(cloudiness precipitation)) {
 		print(" $phenomena{$_}{$forecast->{PHENOMENA}{$_}}" // "неправильный код прогноза");
 	}
-	print " $phenomena{rpower}{$forecast->{PHENOMENA}{rpower}}\n" unless $forecast->{PHENOMENA}{precipitation} == 10;
-	print " $phenomena{spower}{$forecast->{PHENOMENA}{spower}}\n" if $forecast->{PHENOMENA}{precipitation} == 8;
+	print " $phenomena{rpower}{$forecast->{PHENOMENA}{rpower}}" unless $forecast->{PHENOMENA}{precipitation} == 10;
+	print " $phenomena{spower}{$forecast->{PHENOMENA}{spower}}" if $forecast->{PHENOMENA}{precipitation} == 8;
 	
 	printf " %03d..%03d мм.рт.ст.", $forecast->{PRESSURE}{min}, $forecast->{PRESSURE}{max};
 	printf ' %+02d..%+02d°C '."\N{RIGHTWARDS DOUBLE ARROW}".' %+02d..%+02d°C',
